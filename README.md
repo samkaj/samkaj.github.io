8bf0e526342896019ca3c131af4e3f2e8ae47f77
samkaj <kajavasamuel@gmail.com>
2023-11-10 16:42:46 +0100

    Add boilerplate

---
+++ README.md
@@ -0,0 +1,3 @@
+# samkaj.github.io
+
+Welcome to my github pages, this is where you find project showcases, my portfolio, and contact info.
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import "../css/nav.scss"
+    import "../css/main.scss"
+	export const prerender = true;
+</script>
+
+<nav>
+	<ul>
+		<li>samkaj</li>
+	</ul>
+
+	<ul id="mid">
+		<li><a href="#">Projects</a></li>
+		<li><a href="#">Experience</a></li>
+		<li><a href="#">Contact</a></li>
+	</ul>
+
+	<ul id="right">
+		<li><a href="https://github.com/samkaj">GitHub</a></li>
+		<li><a href="https://linkedin.com/samuel-kajava">LinkedIn</a></li>
+		<li>Toggle dark/light</li>
+	</ul>
+</nav>
+<slot />
