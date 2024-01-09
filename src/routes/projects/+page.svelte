8df2e71a93e1fdc3a9ac4fd07365f8d3f67d091d
samkaj <kajavasamuel@gmail.com>
2024-01-09 13:12:52 +0100

    add more projects and mobile support

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -2,15 +2,16 @@
 	import Project from '../../components/project.svelte';
 </script>
 
-<main>
+<h1>personal projects</h1>
+<div>
 	<Project
-		heading={'lakrits.nvim'}
-		description={'Colorscheme for Neovim, written in Lua.'}
-		src={'https://raw.githubusercontent.com/samkaj/lakrits.nvim/main/assets/theme.png'}
-		alt={'screenshot of colorscheme written in lua'}
-		keywords={['Colorscheme', 'Vim', 'Editor']}
-		github={'https://github.com/samkaj/lakrits.nvim'}
-		date={'nov 2023'}
+		heading={'ftp client'}
+		description={'FTP client written in C++, implemented as a CLI.'}
+		src={'ftp.png'}
+		alt={'code snippet of ftp client'}
+		keywords={['FTP', 'C++', 'CLI']}
+		github={'https://github.com/samkaj/ftp'}
+		date={'dec 2023'}
 	/>
 	<Project
 		heading={'apskrift.com'}
@@ -23,17 +24,43 @@
 	/>
 	<Project
 		heading={'nadja'}
-		description={"Playlist generator using Spotify's API"}
+		description={"Playlist generator using Spotify's API."}
 		src={'nadja.png'}
 		alt={'the nadja webapp'}
 		keywords={['Spotify', 'TypeScript', 'React']}
 		github={'https://github.com/samkaj/nadja'}
 		date={'apr 2023'}
 	/>
-</main>
+    <Project
+    	heading={'lakrits.nvim'}
+    	description={'Colorscheme for Neovim, written in Lua.'}
+    	src={'https://raw.githubusercontent.com/samkaj/lakrits.nvim/main/assets/theme.png'}
+    	alt={'screenshot of colorscheme written in lua'}
+    	keywords={['Colorscheme', 'Vim', 'Editor']}
+    	github={'https://github.com/samkaj/lakrits.nvim'}
+    	date={'nov 2023'}
+    />
+</div>
+<h1>school projects</h1>
+<div>
+<Project
+	heading={'static http server'}
+	description={'HTTP server written in Go with optional Proxy implementation.'}
+	keywords={['Golang', 'School']}
+	github={'https://github.com/samkaj/tda596-http'}
+	date={'oct 2023'}
+/>
+<Project
+	heading={'shed'}
+	description={'JavaFX application for browsing Systembolaget\'s products.'}
+	keywords={['Java', 'School', 'Agile']}
+	github={'https://github.com/emilsvennesson/shed'}
+	date={'oct 2021'}
+/>
+</div>
 
 <style lang="scss">
-	main {
+	div {
 		display: grid;
 		grid-template-columns: 1fr 1fr;
 		padding-top: 1em;
@@ -41,13 +68,18 @@
 	}
 
 	h1 {
+        font-size: 2.5em;
 		margin-bottom: 0.5em;
+        border-bottom: 0.5px solid var(--fg-lighter);
 	}
 
 	@media only screen and (max-width: 600px) {
-		main {
+		div {
 			grid-template-columns: 1fr;
-			margin-top: 15%;
+            margin-top: 2em;
 		}
+        h1 {
+            display: none;
+        }
 	}
 </style>
