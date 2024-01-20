1f4c4f9a6a4e597ceed18567ef5717d4620410f0
samkaj <kajavasamuel@gmail.com>
2024-01-20 16:20:44 +0100

    change base

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,6 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/samkaj.github.io' : '',
 			relative: false
 		}
 	}
