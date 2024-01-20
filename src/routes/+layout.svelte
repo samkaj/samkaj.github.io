b7a8c08753860498d79520900b403953af09f370
samkaj <kajavasamuel@gmail.com>
2024-01-20 16:13:31 +0100

    format

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import {base} from '$app/paths';
+	import { base } from '$app/paths';
 	import { onMount } from 'svelte';
 	import '../css/nav.scss';
 	import '../css/main.scss';
@@ -23,9 +23,9 @@
 
 <nav>
 	<ul id="left">
-        <li><a id="home-logo" href="{base}/">samkaj</a></li>
-        <li><a href="{base}/projects">Projects</a></li>
-        <li><a href="{base}/about">About</a></li>
+		<li><a id="home-logo" href="{base}/">samkaj</a></li>
+		<li><a href="{base}/projects">Projects</a></li>
+		<li><a href="{base}/about">About</a></li>
 	</ul>
 	<ul id="right">
 		<li>
