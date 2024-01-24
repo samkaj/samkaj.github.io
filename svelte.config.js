369020f524b328be2fef50132cf9a81a57157847
samkaj <kajavasamuel@gmail.com>
2024-01-24 12:35:23 +0100

    use custom workflow for deployment

--- svelte.config.js
+++ svelte.config.js
@@ -13,8 +13,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		paths: {
-			base: '/samkaj.github.io',
-			relative: true
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
 	}
 };
