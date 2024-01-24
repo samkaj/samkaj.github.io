e168ffc506a37536163e4a87a7df3ec1321c1167
samkaj <kajavasamuel@gmail.com>
2024-01-24 12:57:50 +0100

    deploy

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import {base} from '$app/paths';
+    import { base } from '$app/paths';
 	import { onMount } from 'svelte';
 	import '../css/nav.scss';
 	import '../css/main.scss';
