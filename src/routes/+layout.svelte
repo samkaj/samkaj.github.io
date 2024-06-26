a2da57ad35d44c11b7bbd44d1bb2f6911a998f77
samkaj <kajavasamuel@gmail.com>
2024-06-26 16:49:15 +0200

    [task] remove old site

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,52 +3,24 @@
 	import '../css/nav.scss';
 	import '../css/main.scss';
 	import { themeStore } from '../stores/store';
-	let icon = 'fa-sun';
 
 	function toggleDarkMode() {
 		const newMode = $themeStore === 'dark' ? 'light' : 'dark';
-		icon = newMode === 'dark' ? 'fa-moon' : 'fa-sun';
 		$themeStore = newMode;
 		document.documentElement.setAttribute('data-theme', newMode);
 	}
 
 	onMount(() => {
 		const savedTheme = localStorage.getItem('theme') || 'light';
-		icon = savedTheme === 'dark' ? 'fa-moon' : 'fa-sun';
 		$themeStore = savedTheme;
 		document.documentElement.setAttribute('data-theme', savedTheme);
 	});
 </script>
 
 <nav>
-	<ul id="left">
-		<li><a id="home-logo" href="/">samkaj</a></li>
-		<li><a href="/projects">Projects</a></li>
-		<li><a href="/about">About</a></li>
-	</ul>
-	<ul id="right">
-		<li>
-			<a href="https://linkedin.com/in/samuel-kajava"><i class="fa-brands fa-linkedin-in" /></a>
-		</li>
-		<li><a href="https://github.com/samkaj"><i class="fa-brands fa-github" /></a></li>
-		<li>
-			<button on:click={toggleDarkMode}>
-				<i class="fa-solid fa-circle-half-stroke" />
-			</button>
-		</li>
-	</ul>
 </nav>
-<div class="wrapper">
-	<slot />
-</div>
+<main>
+</main>
 
 <style lang="scss">
-	nav {
-		height: 3em;
-	}
-
-	.wrapper {
-		width: 80vw;
-		margin: auto;
-	}
 </style>
