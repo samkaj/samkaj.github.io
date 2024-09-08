342e5cb7ec0d3ede2576a9a030feb513a7e9d5f9
samkaj <kajavasamuel@gmail.com>
2024-09-08 21:09:18 +0200

    [fix] use base path again

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,8 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: '',
+			relative: true
 		}
 	}
 };
