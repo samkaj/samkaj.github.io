375018aa0d9cb2d0f7757be08a8e8ed59241d9b8
samkaj <kajavasamuel@gmail.com>
2024-09-08 21:22:50 +0200

    [chore] remove stores

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,6 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
 	import '../css/nav.scss';
 	import '../css/main.scss';
-	import { themeStore } from '../stores/store';
-
-	// Dunno if we want light mode
-	function toggleDarkMode() {
-		const newMode = $themeStore === 'dark' ? 'light' : 'dark';
-		$themeStore = newMode;
-		document.documentElement.setAttribute('data-theme', newMode);
-	}
-
-	onMount(() => {
-		const savedTheme = localStorage.getItem('theme') || 'light';
-		$themeStore = savedTheme;
-		document.documentElement.setAttribute('data-theme', savedTheme);
-	});
 </script>
 
 <nav>
